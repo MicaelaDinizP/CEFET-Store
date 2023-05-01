8fba045f372caf94d95a3a985524b7a9623b782f
Marcelli Assumpção (dev) <marcelli.assumpcao@precocerto.co>
2023-05-01 15:18:35 -0300

    test: implementa testes para o script de conexão com o banco

---
+++ api/spec/conexao-pdo.spec.php
@@ -0,0 +1,23 @@
+<?php
+
+use Kahlan\Code\Code;
+
+require_once("vendor/autoload.php");
+require_once("./app/conexao-pdo.php");
+
+describe("conexaoPDO", function(){
+    it("deve criar a conexão corretamente ao enviar o nome cefet-shop.", function() {
+        $pdo = conexaoPDO('cefet-shop');
+        expect($pdo)->toBeAnInstanceOf('PDO');
+    });
+    it("deve lançar PDOException ao enviar o nome de banco de dados inválido.", function() {
+        $lancaPdoException = function(){
+            conexaoPDO('lazaro');
+        };
+        expect($lancaPdoException)->toThrow(new PDOException("SQLSTATE[HY000] [1049] Unknown database 'lazaro'", 1049));
+    });
+
+});
+
+
+?>
