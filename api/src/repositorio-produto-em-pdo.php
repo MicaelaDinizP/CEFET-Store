8cbb2a952d220fad45e8259b4bf050f4a699a253
MicaelaPaes <micaelahimura@hotmail.com>
2023-05-06 19:26:34 -0300

    feat: adiciona implementação para function obterPagina

---
+++ api/src/repositorio-produto-em-pdo.php
@@ -0,0 +1,40 @@
+<?php
+require_once('repositorio-produto-exception.php');
+require_once('repositorio-produto.php');
+require_once('produto.php');
+class RepositorioProdutoEmPDO /*implements RepositorioProduto*/{
+    private $pdo;
+
+    public function __construct( $pdo ){
+        $this->pdo = $pdo;
+    }
+
+    public function obterPagina($pagina, $qtdRegistros){
+        $sql = "SELECT * FROM produto LIMIT :pag  OFFSET :qtdRegistros";
+        $produtos = null;
+        try{
+            $ps = $this->pdo->prepare($sql);
+            $ps->setFetchMode(PDO::FETCH_ASSOC);
+            $ps->bindParam(':pag', $pagina, PDO::PARAM_INT);
+            $ps->bindParam(':qtdRegistros', $qtdRegistros, PDO::PARAM_INT);
+            $ps->execute();
+            if( $ps->rowCount()<0 ){
+                return null;
+            }  
+            $dados = $ps->fetchAll();
+            foreach( $dados as $d ){
+                $produtos[] = new Produto( utf8_encode($d['descricao']), doubleval($d['precoDeVenda']), 
+                    null ,null, null, null, intval($d['id']) ); 
+            }
+            return $produtos;
+
+        }catch( PDOException $e ){
+            throw new RepositorioProdutoException("Não foi possível obter os produtos");
+        }
+    }
+
+}
+
+
+
+?>
