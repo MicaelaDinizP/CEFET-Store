2722a083671af7d4683e9304648a00c725360d88
MicaelaPaes <micaelahimura@hotmail.com>
2023-05-06 20:45:47 -0300

    feat: adiciona VisaoProdutos

---
+++ api/src/visao-produto.php
@@ -0,0 +1,23 @@
+<?php
+
+class VisaoProduto{
+
+    public function produtosEmJson( $produtos ) {
+        return json_encode($produtos);
+    } 
+
+    public function exibirSucesso( $codigo ){
+        http_response_code( $codigo );
+        header('Content-Type: application/json; charset=utf-8');
+    }
+
+    public function exibirErro( $mensagem, $codigo ) {
+        http_response_code( $codigo );
+        header('Content-Type: application/json');
+        return json_encode( $mensagem );
+    }
+
+}
+?>
+
+?>
