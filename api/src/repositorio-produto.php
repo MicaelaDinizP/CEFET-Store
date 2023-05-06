564522ad6fa9c9033d8852473eb8342361edc077
MicaelaPaes <micaelahimura@hotmail.com>
2023-05-06 19:25:25 -0300

    feat: adiciona interface para o repositorio de produtos

---
+++ api/src/repositorio-produto.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once ('produto.php');
+interface RepositorioProduto{
+    public function obterPagina( $pagina, $qtdRegistros );
+    public function obterPorNomeOuId( Produto $produto );
+    public function obterMaisVendidos( $quantidade );
+
+}
+
+
+?>
