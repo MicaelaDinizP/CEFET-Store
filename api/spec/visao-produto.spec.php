1a35f3d412a70b1cc27adc3349b321b7e3daab05
MicaelaPaes <micaelahimura@hotmail.com>
2023-05-06 21:43:09 -0300

    test: adiciona um test case para a VisaoProduto

---
+++ api/spec/visao-produto.spec.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once("vendor/autoload.php");
+require_once("./src/visao-produto.php");
+require_once("./src/produto.php");
+
+describe( "VisaoProduto", function() {
+    it( "deve retornar uma string em JSON ao receber um array de objetos da classe produto", function() {
+        $p = new Produto('Bone Turismo',24.99,'2022-01-01','produto em formato de bone', 10, 0, 'Bone', 0);
+        $produtos = [$p, $p, $p];        
+        $produtosEmJson = new VisaoProduto();
+        $produtosEmJson = $produtosEmJson->produtosEmJson( $produtos );
+        expect( $produtosEmJson )->not->toBeFalsy();
+        expect( json_decode( $produtosEmJson ) )->not->toBeNull();
+    });
+});
+
+
+?>
