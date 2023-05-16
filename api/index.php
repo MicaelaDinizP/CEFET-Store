e11c2e72aa10018417cbf02e0677b5331e2c090e
MicaelaPaes <micaelahimura@hotmail.com>
2023-05-16 00:26:30 -0300

    feat: adiciona a rota para obter detalhes dos produtos

--- api/index.php
+++ api/index.php
@@ -8,10 +8,19 @@ $metodo = $_SERVER['REQUEST_METHOD'];
 
 $controladora = new ControladoraProduto();
 
-if ($metodo === 'GET' && preg_match('/^produtos$/i', $rota)) {
+if ($metodo === 'GET' && preg_match('/^produtos(\?pag=\d{1,5})?$/', $rota)) {
   $produtos = $controladora->obterProdutos();
+  header('Content-Type:application/json;charset=utf-8');
   echo $produtos;
-  die();
+}else if( $metodo === 'GET' && preg_match('/^produto(?:\?id=\d+)?(?:[&\?]descricao=[^&]{1,255})?$/', $rota ) ) {
+    $produto = $controladora->obterPorNomeOuId();
+    echo $produto;
+}else if( $metodo === 'GET' && preg_match('/^mais-vendidos$/i', $rota ) ){
+    $produtos = $controladora->obterMaisVendidos();
+    echo $produtos;
+}else{
+  http_response_code(404);
+  header('Content-Type: application/json');
+  echo json_encode("Desculpe, o recurso que você está procurando não foi encontrado ou a requisição está inválida.");
 }
-
 ?>
