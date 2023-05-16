7f68324c2caa54433114025f896052c53e0b0d41
MicaelaPaes <micaelahimura@hotmail.com>
2023-05-16 00:12:41 -0300

    feat: acrescenta function que obtém id da requisição

--- api/src/visao-produto.php
+++ api/src/visao-produto.php
@@ -8,23 +8,34 @@ class VisaoProduto{
 
     public function exibirSucesso( $codigo ){
         http_response_code( $codigo );
-        header('Content-Type: application/json; charset=utf-8');
+        header('Content-Type:application/json;charset=utf-8');
     }
 
     public function exibirErro( $mensagem, $codigo ) {
         http_response_code( $codigo );
-        header('Content-Type: application/json');
+        header('Content-Type:application/json;charset=utf-8');
         return json_encode( $mensagem );
     }
 
     public function obterPaginaDesejada(){
         if( isset( $_GET['pag'] ) ) {
-            return intval( $_GET['pag'] );
+            return intval( htmlspecialchars($_GET['pag'] ) );
         }
         return 1;
     }
 
-}
-?>
+    public function obterNomeOuId() {
+        $idRequisicao = 0;
+        $nomeRequisicao = null;
+        if( isset( $_GET['id'] ) ) {
+            $idRequisicao = $_GET['id']; 
+        }else if( isset( $_GET['descricao'] ) ){
+            $nomeRequisicao = $_GET['descricao'];
+        }else{
+            return false;
+        }
+        return new Produto( $nomeRequisicao, null, null, null, null, null, null, null, null, $idRequisicao );
+    }
 
+}
 ?>
