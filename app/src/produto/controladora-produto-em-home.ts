587f92c317e2cc9dde275eb5e253cdb169fefac5
Marcelli Assumpção (dev) <marcelli.assumpcao@precocerto.co>
2023-05-17 03:18:34 -0300

    fix: retira lógica de detalhamento da controladora da pagina inicial

--- app/src/produto/controladora-produto-em-home.ts
+++ app/src/produto/controladora-produto-em-home.ts
@@ -1,17 +1,14 @@
 import { VisaoProdutoEmHome } from "./visao-produto-em-home.js";
 import { ProdutoRepositorio } from "./produto-repositorio.js";
-import { VisaoProdutoEmDetalhes } from "./visao-produto-em-detalhes.js";
 //import { Util } from "../util/util";
 
 export class ControladoraProdutoEmHome {
   visaoProdutoEmHome: VisaoProdutoEmHome;
   produtoRepositorio: ProdutoRepositorio;
-  visaoProdutoEmDetalhes: VisaoProdutoEmDetalhes;
 
   constructor() {
     this.visaoProdutoEmHome = new VisaoProdutoEmHome();
     this.produtoRepositorio = new ProdutoRepositorio();
-    this.visaoProdutoEmDetalhes = new VisaoProdutoEmDetalhes();
   }
 
   iniciarListagem() {
@@ -25,17 +22,6 @@ export class ControladoraProdutoEmHome {
       this.visaoProdutoEmHome.montarListagemMaisVendidos(produtos);
     } catch (erro) {
       //Util.mostrarMensagem("Erro ao carregar os produtos. " + erro);
-    }
-  };
-
-  detalharProduto = async (id: number) => {
-    try {
-      console.log("ENTROU EM DETALHAR PRODUTO NA CONTROLADORA");
-      console.log(id);
-      let produto = await this.produtoRepositorio.obterPorId(id);
-      this.visaoProdutoEmDetalhes.montarDetalhamento(produto);
-    } catch (erro) {
-      //Util.mostrarMensagem("Erro ao carregar o produto. " + erro);
     }
   };
 }
