6ec9d3108d2f61a6b24f7e7be5d8dfbd551f1048
MicaelaPaes <micaelahimura@hotmail.com>
2023-05-20 18:08:50 -0300

    fix: agora permite que o id ou descricao sejam informados isoladamente, de forma correta

--- api/src/visao-produto.php
+++ api/src/visao-produto.php
@@ -19,20 +19,24 @@ class VisaoProduto{
 
     public function obterPaginaDesejada(){
         if( isset( $_GET['pag'] ) ) {
-            return intval( htmlspecialchars($_GET['pag'] ) );
+            $pagina = intval( htmlspecialchars($_GET['pag'] ) );
+            if( $pagina!= 0 ){
+                return $pagina;
+            }
         }
         return 1;
     }
-
     public function obterNomeOuId() {
         $idRequisicao = 0;
         $nomeRequisicao = null;
         if( isset( $_GET['id'] ) ) {
             $idRequisicao = intval(htmlspecialchars($_GET['id'])); 
-        }else if( isset( $_GET['descricao'] ) ){
+        }
+        if( isset( $_GET['descricao'] ) ){
             $nomeRequisicao = htmlspecialchars($_GET['descricao']);
-        }else{
-            return false;
+        }
+        if($idRequisicao==0 && $nomeRequisicao==null){
+           return false; 
         }
         return new Produto( $nomeRequisicao, null, null, null, null, null, null, null, null, $idRequisicao );
     }
