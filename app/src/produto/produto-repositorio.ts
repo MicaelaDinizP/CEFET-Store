7fef5e0c9333d76d81d346c1430f0ce88db71068
MicaelaPaes <micaelahimura@hotmail.com>
2023-05-24 15:25:35 -0300

    feat: altera obterTodos com lógica de paginação.

--- app/src/produto/produto-repositorio.ts
+++ app/src/produto/produto-repositorio.ts
@@ -3,45 +3,33 @@ import { ProdutoErro } from "./produto-erro.js";
 import { type } from "os";
 // import { Util } from "../util/util";
 
-const API_PRODUTOS = "http://localhost/2023-1-pis-g3/api/produtos";
 const API_MAIS_VENDIDOS = "http://localhost/2023-1-pis-g3/api/mais-vendidos";
 const API_PRODUTO = "http://localhost/2023-1-pis-g3/api/produto";
 export class ProdutoRepositorio {
-  obterTodos = async () => {
-    let resposta: any = undefined;
+  obterTodos = async (paginaEscolhida: number) => {
+    const API_PRODUTOS = `http://localhost/2023-1-pis-g3/api/produtos?pag=${paginaEscolhida}`;
 
-    await fetch(API_PRODUTOS, {
+    const response = await fetch(API_PRODUTOS, {
       method: "GET",
       headers: { Accept: "application/json" }
-    })
-      .then((response) => {
-        // if (response.status == 401) Util.redirecionarParaLogin();
+    });
 
-        if (response.status == 403)
-          throw new ProdutoErro(
-            "Você não possui permissão para executar esta ação."
-          );
-
-        if (response.status == 404) return false;
-        else return response.json();
-      })
-      .then((produtos) => {
-        resposta = produtos;
-      })
-      .catch((erro) => {
-        throw new ProdutoErro(erro);
-      });
-
-    if (!resposta) {
-      return resposta;
-    } else {
-      let arrayDeObjetosDeProdutos: Produto[] = [];
-      resposta.forEach((r: any) => {
-        arrayDeObjetosDeProdutos.push(this.transformaEmObjeto(r));
-      });
-
-      return arrayDeObjetosDeProdutos;
+    if (!response.ok) {
+      throw new Error("Erro ao obter os produtos.");
     }
+
+    const produtos: any[] = await response.json();
+    const totalPaginas: number = produtos[produtos.length - 1].totalPaginas;
+    const paginaAtual: number = produtos[produtos.length - 1].paginaAtual;
+    const arrayDeObjetosDeProdutos: Produto[] = produtos.map((produto) =>
+      this.transformaEmObjeto(produto)
+    );
+
+    return {
+      arrayDeObjetosDeProdutos,
+      totalPaginas,
+      paginaAtual
+    };
   };
 
   obterProdutosMaisVendidos = async () => {
