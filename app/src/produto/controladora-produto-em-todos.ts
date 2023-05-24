926c1524bf7e88f139c414ddcc2c855269643a70
MicaelaPaes <micaelahimura@hotmail.com>
2023-05-24 15:48:44 -0300

    feat: define página a ser mostrada na visao.

--- app/src/produto/controladora-produto-em-todos.ts
+++ app/src/produto/controladora-produto-em-todos.ts
@@ -2,7 +2,7 @@ import { VisaoProdutoEmTodos } from "./visao-produto-em-todos.js";
 import { ProdutoRepositorio } from "./produto-repositorio.js";
 //import { Util } from "../util/util";
 
-export class ControladoraProdutoEmHome {
+export class ControladoraProdutoEmTodos {
   visaoProdutoEmTodos: VisaoProdutoEmTodos;
   produtoRepositorio: ProdutoRepositorio;
 
@@ -11,20 +11,32 @@ export class ControladoraProdutoEmHome {
     this.produtoRepositorio = new ProdutoRepositorio();
   }
 
-  iniciarListagem() {
-    this.carregarTodos();
+  iniciarListagem(paginaEscolhida: number) {
+    this.carregarTodos(paginaEscolhida);
     //Util.aoClicarEmDeslogar(Util.deslogar);
   }
 
-  carregarTodos = async () => {
+  carregarTodos = async (paginaEscolhida: number) => {
     try {
-      let produtos = await this.produtoRepositorio.obterTodos();
-      this.visaoProdutoEmTodos.montarListagemProdutos(produtos);
+      const produtos = await this.produtoRepositorio.obterTodos(
+        paginaEscolhida
+      );
+      const paginaAtual = paginaEscolhida;
+      const totalPaginas = produtos.totalPaginas;
+
+      debugger;
+
+      this.visaoProdutoEmTodos.montarListagemProdutos(
+        produtos.arrayDeObjetosDeProdutos,
+        paginaAtual,
+        totalPaginas
+      );
     } catch (erro) {
       //Util.mostrarMensagem("Erro ao carregar os produtos. " + erro);
     }
   };
 }
 
-const controladoraProdutoEmHome = new ControladoraProdutoEmHome();
-controladoraProdutoEmHome.iniciarListagem();
+const controladoraProdutoEmTodos = new ControladoraProdutoEmTodos();
+const paginaEscolhida = 1; // Defina aqui o número da página escolhida
+controladoraProdutoEmTodos.iniciarListagem(paginaEscolhida);
