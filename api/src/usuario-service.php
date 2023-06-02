33e421c73fd604d1ce62a8465421bd363c0f93d5
Marcelli Assumpção (dev) <marcelli.assumpcao@precocerto.co>
2023-06-02 14:10:28 -0300

    feat: adiciona serviço de usuário

---
+++ api/src/usuario-service.php
@@ -0,0 +1,23 @@
+<?php
+class ServicoUsuario {
+    public function __construct() {}
+ 
+    public function iniciarSessao() {
+        session_name('cefetshop');
+        session_start();
+    }
+
+    public function salvarIDUsuario($id) {
+        // $this->iniciarSessao();
+        $_SESSION['id'] = $id;
+    }
+
+    public function destruirSessao() {
+        $this->iniciarSessao();
+        unset($_SESSION['id']);
+        unset($_SESSION['logado']);
+        session_destroy();
+    }
+}
+
+?>
