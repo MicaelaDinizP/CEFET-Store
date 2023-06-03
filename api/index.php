164fb832b3318d178d1b16e13dfb452fd8b99741
MicaelaPaes <micaelahimura@hotmail.com>
2023-06-03 15:40:30 -0300

    feat: adiciona rota para cadastro das vendas

--- api/index.php
+++ api/index.php
@@ -1,22 +1,27 @@
 <?php
 
 require_once('src/controladoras/controladora-produto.php');
+require_once('src/controladoras/controladora-venda.php');
 
 $caminho = $_SERVER['REQUEST_URI'];
 $rota = basename($caminho);
 $metodo = $_SERVER['REQUEST_METHOD'];
 
-$controladora = new ControladoraProduto();
+$controladoraProduto = new ControladoraProduto();
+$controladoraVenda = new ControladoraVenda();
 
 if ($metodo === 'GET' && preg_match('/^produtos(\?pag=\d{1,5})?$/', $rota)) {
-  $controladora->obterProdutos();
+  $controladoraProduto->obterProdutos();
 }else if( $metodo === 'GET' && preg_match('/^produto(?:\?id=\d+)?(?:[&\?]descricao=[^&]{1,255})?$/', $rota ) ) {
-    $controladora->obterPorNomeOuId();
+  $controladoraProduto->obterPorNomeOuId();
 }else if( $metodo === 'GET' && preg_match('/^mais-vendidos$/i', $rota ) ){
-    $controladora->obterMaisVendidos();
+  $controladoraProduto->obterMaisVendidos();
+}else if( $metodo === 'POST' && preg_match('/^finalizar-venda$/i', $rota ) ){
+  $controladoraVenda->cadastrarVenda();
 }else{
   http_response_code(404);
   header('Content-Type: application/json;charset=utf-8');
   echo json_encode("Desculpe, o recurso que você está procurando não foi encontrado ou a requisição está inválida.");
 }
+
 ?>
