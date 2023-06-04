fef605db340f3d7e8b5ffe49ed45902481e421f1
Marcelli Assumpção (dev) <marcelli.assumpcao@precocerto.co>
2023-06-04 15:53:33 -0300

    fix: corrige obtenção de usuários para compatibilidade com requisição JSON

--- api/src/visoes/visao-usuario.php
+++ api/src/visoes/visao-usuario.php
@@ -2,26 +2,37 @@
 
 require_once("./src/usuario.php");
 class VisaoUsuario {
+    private $dadosRequisicao;
+
+    public function __construct() {
+        $dadosRequisicao = file_get_contents('php://input');
+        $this->dadosRequisicao = json_decode($dadosRequisicao);
+    }
+
     public function usuarioEmJson($usuario) {
         echo json_encode($usuario);
         die();
     } 
+
     public function exibirSucesso($codigo) {
         http_response_code($codigo);
         header('Content-Type:application/json;charset=utf-8');
     }
+
     public function exibirErro($mensagem, $codigo) {
         http_response_code($codigo);
         header('Content-Type:application/json;charset=utf-8');
         echo json_encode($mensagem);
         die();
     }
+
     public function obterDadosLogin() {
         $usuario = null;
-        if(isset($_POST['login']) && isset($_POST['senha'])) {
-            $login = htmlspecialchars($_POST['login']);
-            $senha = htmlspecialchars($_POST['senha']);
-            $usuario = new Usuario($login,null,$login,$senha,0,0);
+        $login = $this->dadosRequisicao->login;
+        $senha = $this->dadosRequisicao->senha;
+        if(isset($login, $senha)) {         
+            $usuario = new Usuario(htmlspecialchars($login), null, htmlspecialchars($login), 
+                htmlspecialchars($senha), 0, 0);
             return $usuario;
         }
         return null;
