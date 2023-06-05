7f3d9b7a1317f29e1854a0d5abf5eeef3eae54a5
MicaelaPaes <micaelahimura@hotmail.com>
2023-06-05 15:51:48 -0300

    feat: adiciona rotas de login e logout no arquivo das rotas.ts

--- app/src/rotas.ts
+++ app/src/rotas.ts
@@ -2,3 +2,5 @@ export const API_MAIS_VENDIDOS =
   "http://localhost/2023-1-pis-g3/api/mais-vendidos";
 export const API_PRODUTO = "http://localhost/2023-1-pis-g3/api/produto";
 export const API_PRODUTOS = "http://localhost/2023-1-pis-g3/api/produtos";
+export const API_AUTENTICACAO = "http://localhost/2023-1-pis-g3/api/login";
+export const API_DESLOGAR = "http://localhost/2023-1-pis-g3/api/logout";
