672be6e1d74cfe9ddc873114fc63d498c3bb94f1
MicaelaPaes <micaelahimura@hotmail.com>
2023-06-05 16:11:33 -0300

    feat: adiciona novas functions a interface de RepositorioVenda

--- api/src/repositorios/repositorio-venda.php
+++ api/src/repositorios/repositorio-venda.php
@@ -2,6 +2,10 @@
 require_once ('./src/modelos/venda.php');
 interface RepositorioVenda{
     public function cadastrarVenda( Venda $venda, $idUsuario );
+    public function obterVendasPorIdUsuario( $idUsuario );
+    public function checarEstoque($quantidadeDemandada, $idProduto);
+    public function checarSaldo($saldoDemandado, $idUsuario);
+
 }
 
 ?>
