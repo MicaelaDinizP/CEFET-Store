db26e9b98b5d315593dfef913456b7c33d5c3b9d
Marcelli Assumpção (dev) <marcelli.assumpcao@precocerto.co>
2023-06-05 14:53:45 -0300

    fix: conserta chamada de controladora

--- api/index.php
+++ api/index.php
@@ -2,6 +2,7 @@
 
 require_once('src/controladoras/controladora-produto.php');
 require_once('src/controladoras/controladora-venda.php');
+require_once('src/controladoras/controladora-usuario.php');
 
 $caminho = $_SERVER['REQUEST_URI'];
 $rota = basename($caminho);
@@ -9,6 +10,7 @@ $metodo = $_SERVER['REQUEST_METHOD'];
 
 $controladoraProduto = new ControladoraProduto();
 $controladoraVenda = new ControladoraVenda();
+$controladoraUsuario = new ControladoraUsuario();
 
 if ($metodo === 'GET' && preg_match('/^produtos(\?pag=\d{1,5})?$/', $rota)) {
   $controladoraProduto->obterProdutos();
@@ -18,7 +20,6 @@ if ($metodo === 'GET' && preg_match('/^produtos(\?pag=\d{1,5})?$/', $rota)) {
   $controladoraProduto->obterMaisVendidos();
 }else if( $metodo === 'POST' && preg_match('/^finalizar-venda$/i', $rota ) ){
   $controladoraVenda->cadastrarVenda();
-    $controladora->obterMaisVendidos();
 }else if($metodo === 'POST' && preg_match('/^login$/i', $rota)) {
   $controladoraUsuario->login();
 }else if($metodo === 'GET' && preg_match('/^logout$/i', $rota)) {
