3f0f23fe79c7d283c38b6899a02c1207b4fa31bb
Marcelli Assumpção (dev) <marcelli.assumpcao@precocerto.co>
2023-06-06 18:13:40 -0300

    feat: adiciona rota para finalizar venda no arquivo de rotas do front

--- app/src/rotas.ts
+++ app/src/rotas.ts
@@ -4,3 +4,5 @@ export const API_PRODUTO = "http://localhost/2023-1-pis-g3/api/produto";
 export const API_PRODUTOS = "http://localhost/2023-1-pis-g3/api/produtos";
 export const API_AUTENTICACAO = "http://localhost/2023-1-pis-g3/api/login";
 export const API_DESLOGAR = "http://localhost/2023-1-pis-g3/api/logout";
+export const API_FINALIZAR_VENDA =
+  "http://localhost/2023-1-pis-g3/api/finalizar-venda";
