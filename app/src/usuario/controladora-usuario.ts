99c4c9e7158d7ce67a47258c9108828b4a510d10
Marcelli Assumpção (dev) <marcelli.assumpcao@precocerto.co>
2023-06-07 13:27:31 -0300

    feat: adiciona visao e controladora de usuario

---
+++ app/src/usuario/controladora-usuario.ts
@@ -0,0 +1,62 @@
+import { ProdutoRepositorio } from "./../produto/produto-repositorio";
+import { UsuarioRepositorio } from "./usuario-repositorio";
+import { VisaoUsuario } from "./visao-usuario";
+import { ControladoraUsuarioEmAutenticacao } from "../autenticacao/controladora-usuario-em-autenticacao.js";
+
+export class ControladoraUsuario {
+  usuarioRepositorio: UsuarioRepositorio;
+  visaoUsuario: VisaoUsuario;
+  controladoraUsuarioEmAutenticacao: ControladoraUsuarioEmAutenticacao;
+  produtoRepositorio: ProdutoRepositorio;
+
+  constructor() {
+    this.usuarioRepositorio = new UsuarioRepositorio();
+    this.visaoUsuario = new VisaoUsuario();
+    this.controladoraUsuarioEmAutenticacao =
+      new ControladoraUsuarioEmAutenticacao();
+    this.produtoRepositorio = new ProdutoRepositorio();
+  }
+
+  iniciar = () => {
+    this.carregarUsuario();
+    this.carregarCompras();
+    // this.carregarGrafico();
+    this.atualizarBadgeCarrinho();
+    if (this.controladoraUsuarioEmAutenticacao.estaLogado()) {
+      this.visaoUsuario.mostrarUsuarioLogado();
+    } else {
+      this.visaoUsuario.mostrarUsuarioDeslogado();
+    }
+  };
+
+  carregarUsuario = () => {
+    const usuario = this.usuarioRepositorio.pegarUsuarioDaLocalStorage();
+    this.visaoUsuario.mostrarInformacoesUsuario(usuario);
+  };
+
+  carregarCompras = async () => {
+    try {
+      console.log("carregarCompras");
+      const compras = await this.usuarioRepositorio.retornaComprasDoUsuario();
+      console.log("compras");
+      console.log(compras);
+      this.visaoUsuario.mostrarCompras(compras);
+    } catch (erro) {}
+  };
+
+  // carregarGrafico = async () => {
+  //   try {
+  //     const compras = await this.usuarioRepositorio.retornaComprasDoUsuario();
+  //     this.visaoUsuario.criarGraficoCompras(compras);
+  //   } catch (erro) {}
+  // };
+
+  atualizarBadgeCarrinho = () => {
+    const quantidadeProdutos =
+      this.produtoRepositorio.obterQuantidadeProdutos();
+    this.visaoUsuario.atualizarBadgeCarrinho(quantidadeProdutos);
+  };
+}
+
+const controladoraUsuario = new ControladoraUsuario();
+controladoraUsuario.iniciar();
