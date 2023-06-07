0c594adbd054abbc22edf256e4eef8e4494d7160
Marcelli Assumpção (dev) <marcelli.assumpcao@precocerto.co>
2023-06-07 15:06:42 -0300

    refactor: refatora funções do repositório de produto

--- app/src/produto/produto-repositorio.ts
+++ app/src/produto/produto-repositorio.ts
@@ -9,7 +9,7 @@ export class ProdutoRepositorio {
     });
 
     if (!response.ok) {
-      throw new Error("Erro ao obter os produtos.");
+      throw new ProdutoErro("Erro ao obter os produtos.");
     }
 
     const produtos: any[] = await response.json();
@@ -27,63 +27,53 @@ export class ProdutoRepositorio {
   };
 
   obterProdutosMaisVendidos = async () => {
-    let resposta: any = undefined;
-
-    await fetch(API_MAIS_VENDIDOS, {
-      method: "GET",
-      headers: { Accept: "application/json" }
-    })
-      .then((response) => {
-        if (response.status == 403)
-          throw new ProdutoErro(
-            "Você não possui permissão para executar esta ação."
-          );
-
-        if (response.status == 404) return false;
-        else return response.json();
-      })
-      .then((produtos) => {
-        resposta = produtos;
-      })
-      .catch((erro) => {
-        throw new ProdutoErro(erro);
+    try {
+      const response = await fetch(API_MAIS_VENDIDOS, {
+        method: "GET",
+        headers: { Accept: "application/json" }
       });
 
-    if (!resposta) {
-      return resposta;
-    } else {
-      let arrayDeObjetosDeMaisVendidos: Produto[] = [];
-      resposta.forEach((r: any) => {
-        arrayDeObjetosDeMaisVendidos.push(this.transformaEmObjeto(r));
-      });
+      if (response.status === 403) {
+        throw new ProdutoErro(
+          "Você não possui permissão para executar esta ação."
+        );
+      }
+
+      if (response.status === 404) {
+        return [];
+      }
+
+      const produtos = await response.json();
+      const arrayDeObjetosDeMaisVendidos: Produto[] = produtos.map(
+        (produto: any) => this.transformaEmObjeto(produto)
+      );
 
       return arrayDeObjetosDeMaisVendidos;
+    } catch (erro: any) {
+      throw new ProdutoErro(erro);
     }
   };
 
   obterPorId = async (id: number) => {
-    let resposta: any = undefined;
-
-    await fetch(API_PRODUTO + "?id=" + id, {
+    const response = await fetch(API_PRODUTO + "?id=" + id, {
       method: "GET",
       headers: { Accept: "application/json" }
-    })
-      .then((response) => {
-        if (response.status == 403)
-          throw new ProdutoErro(
-            "Você não possui permissão para executar esta ação."
-          );
-        if (response.status == 404) return false;
-        else return response.json();
-      })
-      .then((produto) => {
-        resposta = produto;
-      })
-      .catch((erro) => {
-        throw new ProdutoErro(erro);
-      });
-    resposta = this.transformaEmObjeto(resposta[0]);
-    return resposta;
+    });
+
+    if (response.status === 403) {
+      throw new ProdutoErro(
+        "Você não possui permissão para executar esta ação."
+      );
+    }
+
+    if (response.status === 404) {
+      throw new ProdutoErro("Produto não encontrado.");
+    }
+
+    const resposta = await response.json();
+    const produto = this.transformaEmObjeto(resposta[0]);
+
+    return produto;
   };
 
   transformaEmObjeto(prod: any) {
